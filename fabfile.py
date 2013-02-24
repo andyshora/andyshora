f51c3bd7d88b3b1573f596d8ed27765a38222831
Andy Shora <andyshora@gmail.com>
2013-02-24 19:58:50 +0000

    design copied over

---
+++ fabfile.py
@@ -0,0 +1,49 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+env.roledefs = {
+    'test': ['andy@176.58.118.231:23456'],
+    'prod': ['andy@176.58.115.154:23456']
+}
+
+def test():
+    code_dir = '/home/andy/py'
+    with cd(code_dir):
+        run("uname -n")
+
+# push repo to test environment        
+def push_test():
+    local('sudo git push test')
+
+# push repo to production environment        
+def push_prod():
+    local('sudo git push prod')
+
+def cron_log():
+    run('cat ~/log/cron.log')
+
+def list_rules():
+    run('sudo iptables -L')
+
+def list_jobs():
+    run('sudo cat ls /var/spool/cron/crontabs/root')
+
+#block ip: sudo fab -R prod block_ip:ip=209.85.224.96
+#sudo fab -R prod block_ip:117.206.154.104
+#sudo fab -R prod block_ip:117.206.144.214  
+#sudo fab -R prod block_ip:87.126.253.97
+
+def block_ip(ip):
+    print("Blocking ip: %s" % ip)
+    run("sudo iptables -I INPUT 7 -s %s -j DROP" % ip)
+    run("sudo sh -c 'iptables-save>/home/andy/iptables.rules'")
+
+def restart_nginx():
+    run('sudo /etc/init.d/nginx restart')
+
+def restart_php():
+    run('sudo /etc/init.d/php5-fpm restart')
+
+def access_log(str):
+    run("sudo grep %s /var/log/nginx/access.log.1" % str)
