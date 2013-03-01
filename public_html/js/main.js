219ee8cc82db31c19f3c8aac6562461ac4f9538c
Andy Shora <andyshora@gmail.com>
2013-03-01 22:07:43 +0000

    feature detection

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -3,20 +3,13 @@ var plane_html = "";
 
 function init_animations(){
 	
+	$('#buildings_wrap,#pods_wrap,#clouds').show();
 	$('.plane_lights_wrap').append(plane_html);
 	$('.plane_wrap').addClass('fly');
 	$('#pods').addClass('spin');
 }
 
 $(document).ready(function(){
-	
-	var plane_light_str = '<div class="plane_light"></div>';
-	for(var i=0; i<20; i++)
-		plane_html += plane_light_str;
-
-	plane_html += '<div class="plane_tail_light"></div>';
-	plane_html += '<div class="plane_wing_light"></div>';
-	plane_html += '<div class="plane_btm_light"></div>';
 
 	// PARALLAX
 	/*$(window).bind('scroll.globalMessage', function(){
@@ -31,20 +24,38 @@ $(document).ready(function(){
 		}
 	});*/
 
-	$('#stop_css3').click(function(){
+	if (Modernizr.cssanimations) {
+		var plane_light_str = '<div class="plane_light"></div>';
+		for(var i=0; i<20; i++)
+			plane_html += plane_light_str;
 
-		$('#plane_wrap').removeClass('fly');
-		$('#pods').removeClass('spin');
-		$('#clouds, #buildings_wrap').fadeOut(2000);
-		
-		$(this).hide();
-	});
+		plane_html += '<div class="plane_tail_light"></div>';
+		plane_html += '<div class="plane_wing_light"></div>';
+		plane_html += '<div class="plane_btm_light"></div>';
+
+		$('#stop_css3').click(function(){
+			$('#plane_wrap').removeClass('fly');
+			$('#pods').removeClass('spin');
+			$('#clouds, #buildings_wrap').fadeOut(2000);
+			$(this).hide();
+
+		}).show();
+
+		init_animations();
+	}
+
+	
+
+	Modernizr.load([{
+		test : Modernizr.cssgradients,
+		nope : ['css/gradients.css']
+	}
+	]);
+
 
 	window._gaq = [['_setAccount','UA-17716290-10'],['_trackPageview'],['_trackPageLoadTime']];
     Modernizr.load({
       load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
     });
-
-    setTimeout(function() { init_animations(); }, 1000);
 
 });
