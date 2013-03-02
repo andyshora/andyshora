aa283873df77903ca1fd12f67c8916dcde94fa7f
Andy Shora <andyshora@gmail.com>
2013-03-02 22:11:34 +0000

    mobile styles

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,12 +1,26 @@
 var c2_fixed = false;
 var plane_html = "";
+var window_width = 0;
+var animations_on = false;
 
 function init_animations(){
-	
+	animations_on = true;
 	$('#buildings_wrap,#pods_wrap,#clouds').show();
-	$('.plane_lights_wrap').append(plane_html);
 	$('.plane_wrap').addClass('fly');
 	$('#pods').addClass('spin');
+}
+
+function stop_animations(fade){
+	animations_on = false;
+	$('#plane_wrap').removeClass('fly');
+	$('#pods').removeClass('spin');
+
+	if ((fade!==undefined)&& fade)
+		$('#clouds, #buildings_wrap').fadeOut(2000);
+	else 
+		$('#clouds, #buildings_wrap').hide();
+
+	$('#stop_css3').hide();
 }
 
 $(document).ready(function(){
@@ -33,11 +47,10 @@ $(document).ready(function(){
 		plane_html += '<div class="plane_wing_light"></div>';
 		plane_html += '<div class="plane_btm_light"></div>';
 
+		$('.plane_lights_wrap').append(plane_html);
+
 		$('#stop_css3').click(function(){
-			$('#plane_wrap').removeClass('fly');
-			$('#pods').removeClass('spin');
-			$('#clouds, #buildings_wrap').fadeOut(2000);
-			$(this).hide();
+			stop_animations(true);
 
 		}).show();
 
@@ -56,6 +69,15 @@ $(document).ready(function(){
 	window._gaq = [['_setAccount','UA-17716290-10'],['_trackPageview'],['_trackPageLoadTime']];
     Modernizr.load({
       load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+    });
+
+    $(window).bind('resize', function(){
+    	window_width = $(this).width();
+    	if ((window_width<=480) && animations_on) {
+    		stop_animations(false);
+    	} else if ((window_width>480) && (!animations_on)) {
+    		init_animations();
+    	} 
     });
 
 });
